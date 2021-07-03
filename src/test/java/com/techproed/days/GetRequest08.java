878b528041fd1673eff4a2ea696705cd24fa0a58
Harun <67702854+hrndgc@users.noreply.github.com>
2021-07-03 16:37:31 +0300

    Added new examples

---
+++ src/test/java/com/techproed/days/GetRequest08.java
@@ -0,0 +1,40 @@
+package com.techproed.days;
+
+import com.techproed.testBase.TestBaseRestApiExample;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest08 extends TestBaseRestApiExample {
+    /*
+    http://dummy.restapiexample.com/api/v1/employees url’inde bulunan
+   1) Butun calisanlarin isimlerini consola yazdıralim
+   2) 3. calisan kisinin ismini konsola yazdıralim
+   3) Ilk 5 calisanin adini konsola yazdiralim
+   4) En son calisanin adini konsola yazdiralim
+     */
+
+    @Test
+    public void test01(){
+        spec03.pathParam("param1", "employees");
+
+        Response response = given().
+                accept("application/json").
+                spec(spec03).when().
+                get("/{param1}");
+
+        //response.prettyPrint();
+
+        JsonPath json=response.jsonPath();
+        System.out.println(json.getList("data.employee_name"));
+        // response daki tüm employee leri bulur ve getirir.
+        System.out.println(json.getString("data.employee_name[2]"));
+        System.out.println(json.getString("data.employee_name[0,1,2,3,4]"));
+        System.out.println(json.getString("data.employee_name[-1]"));
+    }
+
+}
---
+++ src/test/java/com/techproed/testBase/TestBaseRestApiExample.java
@@ -0,0 +1,18 @@
+package com.techproed.testBase;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class TestBaseRestApiExample {
+
+    protected RequestSpecification spec03;
+
+    @Before
+    public void setup(){
+
+        spec03 = new RequestSpecBuilder().
+                setBaseUri("http://dummy.restapiexample.com/api/v1").
+                build();
+    }
+}
